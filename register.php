14dc29033ae769ab675433c0d49adb70d039589b
Anurag Sharma <annurag94@gmail.com>
2021-03-15 21:00:52 +0530

    Add register.php

---
+++ register.php
@@ -0,0 +1,17 @@
+<style>
+    input {
+        margin: 10px;
+    }
+</style>
+
+<h1>Welcome to our website!</h1>
+
+Please use this form to register:
+
+<form action="register_user.php" method="post">
+	Name: <input type="text" name="name" placeholder="Enter your name"><br>
+	Email: <input type="text" name="email" placeholder="Enter your email"><br>
+	Password: <input type="password" name="password" placeholder="Enter password"><br>
+	DOB: <input type="date" name="dob" placeholder="Enter dob"><br>
+	<input type="submit">
+</form>
